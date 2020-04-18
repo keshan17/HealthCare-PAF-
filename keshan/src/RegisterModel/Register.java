7facad3b881e505aee2ee22643052ee55c8a2655
keshan weragoda <keshanweragoda1@gmail.com>
2020-04-18 12:58:03 +0530

    register db connection

---
+++ keshan/src/RegisterModel/Register.java
@@ -0,0 +1,26 @@
+package RegisterModel;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class Register {
+	public Connection connect()
+	{
+		Connection con=null;
+		try
+		{
+			Class.forName("com.mysql.jdbc.Driver");
+			con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/newdb", "root", ""); 
+			System.out.print("Successfully connected"); 
+			
+			
+		}
+		catch(Exception e)
+		{
+			System.out.print("error");
+			e.printStackTrace(); 
+		}
+		return con;
+	}
+
+}
